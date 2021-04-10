a9558a1e375aeefdc8460601c6c58c4ea2b0bd3b
Wouter Janssens <wouter@digita.ai>
2021-04-10 19:55:30 +0200

    build: basic project setup (#6)
    * feat: support building and testing semantic components
    
    * chore: renamed nde-erfgoedinstellingen to nde-erfgoed
    
    * feat: added shared theme package
    
    * feat: added core library
    
    * build: added basic ci workflow
    
    * feat: added pod server

---
+++ packages/nde-erfgoed-components/jest.config.ts
@@ -0,0 +1,21 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'jest-environment-jsdom-sixteen',
+  setupFiles: ['<rootDir>/tests/setup.ts'],
+  transformIgnorePatterns: ['node_modules/(?!(lit-element|lit-html)/)'],
+  moduleNameMapper: {
+    '~(.*)': '<rootDir>/lib/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      diagnostics: {
+        warnOnly: true,
+      },
+    },
+  },
+};
+
+export default config;
---
+++ packages/nde-erfgoed-components/lib/collections/collections.component.ts
@@ -0,0 +1,58 @@
+import { LitElement, css, html, property } from 'lit-element';
+import type { Component } from '@digita-ai/semcom-core';
+import { Collection } from '@digita-ai/nde-erfgoed-core';
+
+/**
+ * A component which shows an summary of multiple collections.
+ */
+export default class CollectionsComponent extends LitElement implements Component {
+
+  /**
+   * The collections which will be summarized by the component.
+   */
+  @property({type: Array})
+  private collections: Collection[] = null;
+
+  /**
+   * The styles associated with the component.
+   */
+  static get styles() {
+    return [
+      css`
+        .collection { }
+      `,
+    ];
+  }
+
+  /**
+   * Loads data associated with the component.
+   *
+   * @param entry The resource which will be loaded by the component.
+   * @param customFetch A custom fetch function provided by the host application.
+   * @returns A promise when the data has been loaded.
+   */
+  data (entry: string, customFetch?: (input: RequestInfo, init?: RequestInit) => Promise<Response>): Promise<void> {
+    const myFetch = customFetch ? customFetch : fetch;
+
+    return myFetch(entry)
+      .then((response) => response.text())
+      .then(() => {
+        this.collections = [];
+      });
+
+  }
+
+  /**
+   * Renders the component as HTML.
+   *
+   * @returns The rendered HTML of the component.
+   */
+  render() {
+    return html`
+    <link href="./dist/styles.css" rel="stylesheet">
+    <div class="collections">
+    ${this.collections.map((collection) => html`<nde-collection collection='${JSON.stringify(collection)}'></nde-collection>`)}
+    </div>
+  `;
+  }
+}
