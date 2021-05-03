37e3eecce11c15617871466e98bdaed4a86f26a5
Wouter Janssens <wouter@digita.ai>
2021-05-03 07:32:20 +0200

    feat: add basic primary navigation (#101)
    * feat: added basic sidebar component
    
    * feat: show sidebar when authenticated
    
    * test: added tests for primary navigation
    
    * docs: update code documentation

---
+++ packages/nde-erfgoed-components/lib/sidebar/sidebar.component.ts
@@ -0,0 +1,58 @@
+import { css, html, property, unsafeCSS } from 'lit-element';
+import { Translator, Logger } from '@digita-ai/nde-erfgoed-core';
+import { RxLitElement } from 'rx-lit';
+import { Theme } from '@digita-ai/nde-erfgoed-theme';
+
+/**
+ * A component which represents a sidebar.
+ */
+export class SidebarComponent extends RxLitElement {
+
+  /**
+   * The component's translator.
+   */
+  @property({type: Translator})
+  public translator: Translator;
+
+  /**
+   * The component's translator.
+   */
+  @property({type: Logger})
+  public logger: Logger;
+
+  /**
+   * Renders the component as HTML.
+   *
+   * @returns The rendered HTML of the component.
+   */
+  render() {
+    return html`
+    <div class="sidebar primary">
+      <slot></slot>
+    </div>
+  `;
+  }
+
+  /**
+   * The styles associated with the component.
+   */
+  static get styles() {
+    return [
+      unsafeCSS(Theme),
+      css`
+        :host {
+          width: var(--size-sidebar);
+          display: flex;
+          flex-direction: column;
+        }
+        .sidebar {
+          flex: 1 0;
+        }
+        .sidebar.primary {
+          background-color: var(--colors-primary-dark);
+          color: var(--colors-foreground-inverse);
+        }
+      `,
+    ];
+  }
+}
--- packages/nde-erfgoed-manage/lib/features/authenticate/authenticate-root.component.ts
+++ packages/nde-erfgoed-manage/lib/features/authenticate/authenticate-root.component.ts
@@ -77,7 +77,7 @@ export class AuthenticateRootComponent extends RxLitElement {
 
       if(this.actor.parent) {
         this.subscribe('alerts', from(this.actor.parent)
-          .pipe(map((state) => state.context.alerts)));
+          .pipe(map((state) => state.context?.alerts)));
       }
     }
 
@@ -159,8 +159,8 @@ export class AuthenticateRootComponent extends RxLitElement {
           justify-content: center;
           align-items: center;
           gap: var(--gap-huge);
+          background-color: var(--colors-primary-dark);
         }
-
         
         .title-container {
           height: 50px;
