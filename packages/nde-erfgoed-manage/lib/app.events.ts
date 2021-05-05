95e0c1da1e90cb3cb8b2e70115a0c2f40a974264
Wouter Termont <woutermont@gmail.com>
2021-05-05 21:40:55 +0200

    chore: eslint and jest fix after rebase

--- packages/nde-erfgoed-manage/lib/app.events.ts
+++ packages/nde-erfgoed-manage/lib/app.events.ts
@@ -115,9 +115,9 @@ export const dismissAlert = choose<AppContext, DismissAlertEvent>([
 /**
  * Action which sets a session in the machine's context.
  */
-export const setSession = assign({session: (context, event: LoggedInEvent) => event.session});
+export const setSession = assign({ session: (context, event: LoggedInEvent) => event.session });
 
 /**
  * Action which removes a session in the machine's context.
  */
-export const removeSession = assign({session: (context, event) => undefined});
+export const removeSession = assign({ session: (context, event) => undefined });
