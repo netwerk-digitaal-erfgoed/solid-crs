0e9efbdc65e408c8598a764e0f273ccf7c46b3e3
Wouter Termont <woutermont@gmail.com>
2021-05-06 15:31:31 +0200

    build: config from central repo (#148)
    * build: root npmrc
    
    * build: commitlint & eslint setup
    
    * build: eslint fixes (errors)
    
    * build: jest config
    
    * build: git hooks
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>
    
    * build: root jest config fix
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>
    
    * build: lerna config
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>
    
    * build: typescript config
    
    Signed-off-by: Wouter Termont <woutermont@gmail.com>
    
    * chore: eslint and jest fix after rebase
    
    * build: ci scripts and package locks

--- packages/nde-erfgoed-manage/lib/app.root.spec.ts
+++ packages/nde-erfgoed-manage/lib/app.root.spec.ts
@@ -8,28 +8,38 @@ import { SolidMockService } from './common/solid/solid-mock.service';
 const solid = new SolidMockService(new ConsoleLogger(LoggerLevel.silly, LoggerLevel.silly));
 
 describe('AppRootComponent', () => {
+
   let component: AppRootComponent;
   let machine: Interpreter<AppContext>;
 
   beforeEach(() => {
+
     machine = interpret(appMachine(solid));
     machine.start();
     component = window.document.createElement('nde-app-root') as AppRootComponent;
 
     component.actor = machine;
+
   });
 
   afterEach(() => {
+
     document.getElementsByTagName('html')[0].innerHTML = '';
+
   });
 
   it('should be correctly instantiated', () => {
+
     expect(component).toBeTruthy();
+
   });
 
   it('should show primary navigation when authenticated', async (done) => {
+
     machine.onTransition(async (state) => {
-      if(state.matches({[AppRootStates.AUTHENTICATE]: AppAuthenticateStates.AUTHENTICATED})){
+
+      if(state.matches({ [AppRootStates.AUTHENTICATE]: AppAuthenticateStates.AUTHENTICATED })){
+
         await component.updateComplete;
         const sidebar = window.document.body.getElementsByTagName('nde-app-root')[0].shadowRoot.querySelectorAll('nde-sidebar');
 
@@ -37,28 +47,38 @@ describe('AppRootComponent', () => {
         expect(sidebar.length).toBe(1);
 
         done();
+
       }
+
     });
 
     window.document.body.appendChild(component);
     await component.updateComplete;
 
-    machine.send({type: AppEvents.LOGGED_IN, session: {webId:'test'}});
+    machine.send({ type: AppEvents.LOGGED_IN, session: { webId:'test' } });
+
   });
 
   it('should not show primary navigation when unauthenticated', async (done) => {
+
     machine.onTransition(async (state) => {
-      if(state.matches({[AppRootStates.AUTHENTICATE]: AppAuthenticateStates.UNAUTHENTICATED})){
+
+      if(state.matches({ [AppRootStates.AUTHENTICATE]: AppAuthenticateStates.UNAUTHENTICATED })){
+
         await component.updateComplete;
         const sidebar = window.document.body.getElementsByTagName('nde-app-root')[0].shadowRoot.querySelectorAll('nde-sidebar');
 
         expect(sidebar.length).toBe(0);
 
         done();
+
       }
+
     });
 
     window.document.body.appendChild(component);
     await component.updateComplete;
+
   });
+
 });
