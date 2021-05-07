5dc44ee3ea5dba87199c7d199ddc2019bd4e7b8f
Arthur Joppart <arthur@digita.ai>
2021-05-07 10:42:07 +0200

    test: register components for tests

--- packages/nde-erfgoed-components/tests/setup.ts
+++ packages/nde-erfgoed-components/tests/setup.ts
@@ -3,6 +3,9 @@ import { CollectionsComponent } from '../lib/collections/collections.component';
 import { AlertComponent } from '../lib/alerts/alert.component';
 import { FormElementComponent } from '../lib/forms/form-element.component';
 import { ContentHeaderComponent } from '../lib/header/content-header.component';
+import { NDECard } from '../lib/collections/nde-card.component';
+import { CollectionCardComponent } from '../lib/collections/collection-card.component';
+import { CollectionObjectCardComponent } from '../lib/collections/collection-object-card.component';
 
 /**
  * Register tags for components.
@@ -12,3 +15,6 @@ customElements.define('nde-collections', CollectionsComponent);
 customElements.define('nde-alert', AlertComponent);
 customElements.define('nde-form-element', FormElementComponent);
 customElements.define('nde-content-header', ContentHeaderComponent);
+customElements.define('nde-collection-object-card', CollectionObjectCardComponent);
+customElements.define('nde-collection-card', CollectionCardComponent);
+customElements.define('nde-card', NDECard);
