c8f066add9ca9d4a8cdcf873dd097cd6c073dc62
Stijn Taelemans <stijn@digita.ai>
2021-05-10 18:49:01 +0200

    chore: added event to create object button + i18n

--- packages/nde-erfgoed-manage/lib/features/collection/collection-root.component.ts
+++ packages/nde-erfgoed-manage/lib/features/collection/collection-root.component.ts
@@ -146,10 +146,10 @@ export class CollectionRootComponent extends RxLitElement {
           <div class="empty-container">
             <div class='empty'>
               ${unsafeSVG(Empty)}
-              <div class='text'>Maak je eerste object aan</div>
-              <button class='accent'>
+              <div class='text'>${this.translator.translate('nde.features.collections.root.empty.create-object-title')}</div>
+              <button class='accent' @click="${() => this.actor.send(CollectionEvents.CLICKED_CREATE_OBJECT)}">
                 ${unsafeSVG(ObjectIcon)}
-                <span>Object aanmaken</span>
+                <span>${this.translator.translate('nde.features.collections.root.empty.create-object-button')}</span>
               </button>
             </div>
           </div>
