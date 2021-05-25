36e8862e9688d679b822bc6231ef3f5804f9331a
Wouter Janssens <wouter@digita.ai>
2021-05-25 10:43:02 +0200

    chore: rename to solid-crs

---
+++ packages/solid-crs-manage/lib/app.machine.spec.ts
@@ -0,0 +1,313 @@
+import { Alert } from '@netwerk-digitaal-erfgoed/solid-crs-components';
+import { Collection, ConsoleLogger, LoggerLevel, MemoryStore, CollectionObjectMemoryStore } from '@netwerk-digitaal-erfgoed/solid-crs-core';
+import { interpret, Interpreter } from 'xstate';
+import { AppEvents, LoggedInEvent } from './app.events';
+import { AppContext, AppDataStates, appMachine, AppRootStates } from './app.machine';
+import { SolidMockService } from './common/solid/solid-mock.service';
+import { CollectionEvents } from './features/collection/collection.events';
+
+describe('AppMachine', () => {
+
+  const collection1 = {
+    uri: 'collection-uri-1',
+    name: 'Collection 1',
+    description: 'This is collection 1',
+  };
+
+  const collection2 = {
+    uri: 'collection-uri-2',
+    name: 'Collection 2',
+    description: 'This is collection 2',
+  };
+
+  let machine: Interpreter<AppContext>;
+
+  beforeEach(() => {
+
+    machine = interpret<AppContext>(
+      appMachine(
+        new SolidMockService(new ConsoleLogger(LoggerLevel.silly, LoggerLevel.silly)),
+        new MemoryStore<Collection>([ collection1, collection2 ]),
+        new CollectionObjectMemoryStore([
+          {
+            uri: 'object-uri-1',
+            name: 'Object 1',
+            description: 'This is object 1',
+            image: null,
+            subject: null,
+            type: null,
+            updated: 0,
+            collection: 'collection-uri-1',
+          },
+        ])
+      )
+        .withContext({
+          alerts: [],
+        }),
+    );
+
+  });
+
+  it('should be correctly instantiated', () => {
+
+    expect(machine).toBeTruthy();
+
+  });
+
+  it('should add alert to context when sending addAlert', () => {
+
+    const alert = { type: 'success', message: 'foo' };
+    machine.start();
+    machine.send(AppEvents.ADD_ALERT, { alert });
+
+    expect(machine.state.context.alerts).toBeTruthy();
+    expect(machine.state.context.alerts.length).toBe(1);
+    expect(machine.state.context.alerts[0]).toEqual(alert);
+
+  });
+
+  it('should not add duplicate alert to context when sending addAlert', () => {
+
+    const alert: Alert = { type: 'success', message: 'foo' };
+
+    machine = interpret<AppContext>(appMachine(
+      new SolidMockService(new ConsoleLogger(LoggerLevel.silly, LoggerLevel.silly)),
+      new MemoryStore<Collection>([ collection1, collection2 ]),
+      new CollectionObjectMemoryStore([
+        {
+          uri: 'object-uri-1',
+          name: 'Object 1',
+          description: 'This is object 1',
+          image: null,
+          subject: null,
+          type: null,
+          updated: 0,
+          collection: 'collection-uri-1',
+        },
+      ])
+    ).withContext({
+      alerts: [ alert ],
+    }));
+
+    machine.start();
+    machine.send(AppEvents.ADD_ALERT, { alert });
+
+    expect(machine.state.context.alerts).toBeTruthy();
+    expect(machine.state.context.alerts.length).toBe(1);
+    expect(machine.state.context.alerts[0]).toEqual(alert);
+
+  });
+
+  it('should send error event when sending addAlert without payload', async (done) => {
+
+    machine.start();
+
+    machine.onEvent(((event) => {
+
+      if(event.type === AppEvents.ERROR) {
+
+        done();
+
+      }
+
+    }));
+
+    machine.send(AppEvents.ADD_ALERT, {});
+
+  });
+
+  it('should dismiss alert in context when sending dismissAlert', () => {
+
+    const alert: Alert = { type: 'success', message: 'foo' };
+
+    machine = interpret<AppContext>(appMachine(
+      new SolidMockService(new ConsoleLogger(LoggerLevel.silly, LoggerLevel.silly)),
+      new MemoryStore<Collection>([ collection1, collection2 ]),
+      new CollectionObjectMemoryStore([
+        {
+          uri: 'object-uri-1',
+          name: 'Object 1',
+          description: 'This is object 1',
+          image: null,
+          subject: null,
+          type: null,
+          updated: 0,
+          collection: 'collection-uri-1',
+        },
+      ])
+    )
+      .withContext({
+        alerts: [ alert ],
+      }));
+
+    machine.start();
+    expect(machine.state.context.alerts.length).toBe(1);
+    machine.send(AppEvents.DISMISS_ALERT, { alert });
+    expect(machine.state.context.alerts.length).toBe(0);
+
+  });
+
+  it('should not dismiss non-existing alert in context when sending dismissAlert', () => {
+
+    const alert: Alert = { type: 'success', message: 'foo' };
+    machine.start();
+    expect(machine.state.context.alerts.length).toBe(0);
+    machine.send(AppEvents.DISMISS_ALERT, { alert });
+    expect(machine.state.context.alerts.length).toBe(0);
+
+  });
+
+  it('should send error event when sending dismissAlert without payload', async (done) => {
+
+    machine.start();
+
+    machine.onEvent(((event) => {
+
+      if(event.type === AppEvents.ERROR) {
+
+        done();
+
+      }
+
+    }));
+
+    machine.send(AppEvents.DISMISS_ALERT, {});
+
+  });
+
+  it('should send add alert event when error event is sent', async (done) => {
+
+    machine.start();
+
+    machine.onEvent(((event) => {
+
+      if(event.type === AppEvents.ADD_ALERT) {
+
+        done();
+
+      }
+
+    }));
+
+    machine.send(AppEvents.ERROR);
+
+  });
+
+  it('should assign session when logged in', async (done) => {
+
+    machine.onChange((context) => {
+
+      if(context.session?.webId === 'lorem') {
+
+        done();
+
+      }
+
+    });
+
+    machine.start();
+
+    machine.send({ type: AppEvents.LOGGED_IN, session: { webId: 'lorem' } } as LoggedInEvent);
+
+  });
+
+  it('should remove session when logged out', async (done) => {
+
+    const solid = new SolidMockService(new ConsoleLogger(LoggerLevel.silly, LoggerLevel.silly));
+
+    machine = interpret<AppContext>(
+      appMachine(solid,
+        new MemoryStore<Collection>([ collection1, collection2 ]),
+        new CollectionObjectMemoryStore([
+          {
+            uri: 'object-uri-1',
+            name: 'Object 1',
+            description: 'This is object 1',
+            image: null,
+            subject: null,
+            type: null,
+            updated: 0,
+            collection: 'collection-uri-1',
+          },
+        ]))
+        .withContext({
+          alerts: [],
+          session: { webId: 'lorem' },
+        }),
+    );
+
+    machine.onChange((context) => {
+
+      if(!context.session) {
+
+        done();
+
+      }
+
+    });
+
+    machine.start();
+
+    machine.send(AppEvents.LOGGED_IN, { session: { webId: 'foo' } });
+    machine.send(AppEvents.LOGGING_OUT);
+
+  });
+
+  it('should send logged in when authenticate machine is done', async (done) => {
+
+    const solid = new SolidMockService(new ConsoleLogger(LoggerLevel.silly, LoggerLevel.silly));
+    solid.getSession = jest.fn(async () => ({ webId: 'lorem' }));
+
+    machine = interpret<AppContext>(
+      appMachine(solid,
+        new MemoryStore<Collection>([ collection1, collection2 ]),
+        new CollectionObjectMemoryStore([
+          {
+            uri: 'object-uri-1',
+            name: 'Object 1',
+            description: 'This is object 1',
+            image: null,
+            subject: null,
+            type: null,
+            updated: 0,
+            collection: 'collection-uri-1',
+          },
+        ]))
+        .withContext({
+          alerts: [],
+        }),
+    );
+
+    machine.onEvent((event) => {
+
+      if(event.type === AppEvents.LOGGED_IN && (event as LoggedInEvent).session?.webId === 'lorem') {
+
+        done();
+
+      }
+
+    });
+
+    machine.start();
+
+  });
+
+  it('should emit selected collection after login', async (done) => {
+
+    machine.onEvent((event) => {
+
+      if(event.type === CollectionEvents.SELECTED_COLLECTION) {
+
+        done();
+
+      }
+
+    });
+
+    machine.start();
+
+    machine.send(AppEvents.LOGGED_IN, { session: { webId: 'foo' } });
+
+  });
+
+});
---
+++ packages/solid-crs-manage/lib/features/object/object-root.component.ts
@@ -0,0 +1,217 @@
+import { html, property, PropertyValues, internalProperty, unsafeCSS, css, TemplateResult, CSSResult } from 'lit-element';
+import { ArgumentError, CollectionObject, Logger, Translator } from '@netwerk-digitaal-erfgoed/solid-crs-core';
+import { FormEvent, FormActors, FormSubmissionStates, FormEvents, Alert } from '@netwerk-digitaal-erfgoed/solid-crs-components';
+import { map } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { ActorRef, Interpreter, State } from 'xstate';
+import { RxLitElement } from 'rx-lit';
+import { Cross, Object as ObjectIcon, Save, Theme, Trash } from '@netwerk-digitaal-erfgoed/solid-crs-theme';
+import { unsafeSVG } from 'lit-html/directives/unsafe-svg';
+import { AppEvents } from '../../app.events';
+import { ObjectContext, ObjectStates } from './object.machine';
+import { ObjectEvents } from './object.events';
+
+/**
+ * The root page of the object feature.
+ */
+export class ObjectRootComponent extends RxLitElement {
+
+  /**
+   * The component's logger.
+   */
+  @property({ type: Object })
+  public logger: Logger;
+
+  /**
+   * The component's translator.
+   */
+  @property({ type: Object })
+  public translator: Translator;
+
+  /**
+   * The actor controlling this component.
+   */
+  @property({ type: Object })
+  public actor: Interpreter<ObjectContext>;
+
+  /**
+   * The component's alerts.
+   */
+  @property({ type: Array })
+  public alerts: Alert[];
+
+  /**
+   * The state of this component.
+   */
+  @internalProperty()
+  state?: State<ObjectContext>;
+
+  /**
+   * The object to be displayed and/or edited.
+   */
+  @internalProperty()
+  object?: CollectionObject;
+
+  /**
+   * The actor responsible for form validation in this component.
+   */
+  @internalProperty()
+  formActor: ActorRef<FormEvent>;
+
+  /**
+   * Indicates if the form is being submitted.
+   */
+  @internalProperty()
+  isSubmitting? = false;
+
+  /**
+   * Hook called on at every update after connection to the DOM.
+   */
+  updated(changed: PropertyValues): void {
+
+    super.updated(changed);
+
+    if(changed && changed.has('actor') && this.actor){
+
+      if(this.actor.parent){
+
+        this.subscribe('alerts', from(this.actor.parent)
+          .pipe(map((state) => state.context?.alerts)));
+
+      }
+
+      this.subscribe('formActor', from(this.actor).pipe(
+        map((state) => state.children[FormActors.FORM_MACHINE]),
+      ));
+
+      if(changed.has('formActor') && this.formActor){
+
+        this.subscribe('isSubmitting', from(this.formActor).pipe(
+          map((state) => state.matches(FormSubmissionStates.SUBMITTING)),
+        ));
+
+      }
+
+      this.subscribe('state', from(this.actor));
+
+      this.subscribe('object', from(this.actor)
+        .pipe(map((state) => state.context?.object)));
+
+    }
+
+  }
+
+  /**
+   * Handles a dismiss event by sending a dismiss alert event to its parent.
+   *
+   * @param event Dismiss event dispatched by an alert componet.
+   */
+  handleDismiss(event: CustomEvent<Alert>): void {
+
+    if (!event || !event.detail) {
+
+      throw new ArgumentError('Argument event || event.detail should be set.', event);
+
+    }
+
+    if (!this.actor || !this.actor.parent) {
+
+      throw new ArgumentError('Argument this.actor || !this.actor.parent should be set.', this.actor);
+
+    }
+
+    this.actor.parent.send(AppEvents.DISMISS_ALERT, { alert: event.detail });
+
+  }
+
+  /**
+   * Renders the component as HTML.
+   *
+   * @returns The rendered HTML of the component.
+   */
+  render(): TemplateResult {
+
+    // Create an alert components for each alert.
+    const alerts = this.alerts?.map((alert) => html`<nde-alert .logger='${this.logger}' .translator='${this.translator}' .alert='${alert}' @dismiss="${this.handleDismiss}"></nde-alert>`);
+
+    return this.object ? html`
+    <nde-content-header inverse>
+      <div slot="icon">${ unsafeSVG(ObjectIcon) }</div>
+      ${this.state.matches(ObjectStates.EDITING)
+    ? html`
+          <nde-form-element slot="title" .inverse="${true}" .showLabel="${false}" .actor="${this.formActor}" .translator="${this.translator}" field="name">
+            <input autofocus type="text" slot="input" class="name" value="${this.object.name}"/>
+          </nde-form-element>
+          <nde-form-element slot="subtitle" .inverse="${true}" .showLabel="${false}" .actor="${this.formActor}" .translator="${this.translator}" field="description">
+            <input type="text" slot="input" class="description" value="${this.object.description}"/>
+          </nde-form-element>
+        `
+    : html`
+          <div slot="title" @click="${() => this.actor.send(ObjectEvents.CLICKED_EDIT)}">
+            ${this.object.name}
+          </div>
+          <div slot="subtitle" @click="${() => this.actor.send(ObjectEvents.CLICKED_EDIT)}">
+            ${this.object.description}
+          </div>
+        `
+}
+
+      ${ this.state.matches(ObjectStates.EDITING) ? html`<div slot="actions"><button class="no-padding inverse save" @click="${() => this.formActor.send(FormEvents.FORM_SUBMITTED)}" ?disabled="${this.isSubmitting}">${unsafeSVG(Save)}</button></div>` : '' }
+      ${ this.state.matches(ObjectStates.EDITING) ? html`<div slot="actions"><button class="no-padding inverse cancel" @click="${() => this.actor.send(ObjectEvents.CANCELLED_EDIT)}">${unsafeSVG(Cross)}</button></div>` : '' }
+      <div slot="actions"><button class="no-padding inverse delete" @click="${() => this.actor.send(ObjectEvents.CLICKED_DELETE, { object: this.object })}">${unsafeSVG(Trash)}</button></div>
+    </nde-content-header>
+    <div class="content">
+      ${ alerts }
+      
+      FORMULIEREN
+
+    </div>
+  ` : html` this.object is undefined, dit zou niet mogen voorvallen`;
+
+  }
+
+  static get styles(): CSSResult[] {
+
+    return [
+      unsafeCSS(Theme),
+      css`
+        *::-webkit-scrollbar-thumb {
+          background-color: var(--colors-foreground-light);
+          border: 3px solid var(--colors-background-normal);
+        }
+        *::-webkit-scrollbar-track {
+          background: var(--colors-background-normal);
+        }
+        :host {
+          scrollbar-color: var(--colors-foreground-light) var(--colors-background-normal);
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+        }
+        .content {
+          margin-top: 1px;
+          padding: var(--gap-large);
+          height: 100%;
+          overflow-y: auto;
+        }
+        nde-content-header nde-form-element input {
+          height: var(--gap-normal);
+          padding: 0;
+          line-height: var(--gap-normal);
+        }
+        nde-content-header div[slot="title"]:hover, nde-content-header div[slot="subtitle"]:hover {
+          cursor: pointer;
+        }
+        .name {
+          font-weight: bold;
+          font-size: var(--font-size-large);
+        }
+        .description {
+          margin-top: var(--gap-tiny);
+        }
+      `,
+    ];
+
+  }
+
+}
