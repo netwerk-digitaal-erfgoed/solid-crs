861ecf82e8afec7031b7a94bbd3694b3bae74c9f
Stijn Taelemans <stijn@digita.ai>
2021-06-08 12:09:12 +0200

    chore: updated semcom-node uri

--- packages/solid-crs-manage/lib/common/semcom/semcom.service.ts
+++ packages/solid-crs-manage/lib/common/semcom/semcom.service.ts
@@ -5,7 +5,7 @@ export class SemComService {
 
   private registry: AbstractRegisterComponentService = new RegisterComponentService();
 
-  private repo: QueryComponentService = new QueryComponentRemoteService('http://localhost:3003');
+  private repo: QueryComponentService = new QueryComponentRemoteService('https://solid-crs.netwerkdigitaalerfgoed.nl/semcom');
 
   queryComponents(filter: Partial<ComponentMetadata>): Promise<ComponentMetadata[]> {
 
