05b7f9a78bbfd543b24531e6eef37e2d2aed9dc7
Stijn Taelemans <stijn@digita.ai>
2021-06-14 10:00:41 +0200

    fix: made semcom node uri dynamic

---
+++ packages/solid-crs-manage/vite.build.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig( ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  // expose .env as process.env instead of import.meta since jest does not import meta yet
+  const envWithProcessPrefix = Object.entries(env).reduce(
+    (prev, [key, val]) => {
+      return {
+        ...prev,
+        ['process.env.' + key]: `"${val}"`,
+      }
+    },
+    {},
+  );
+
+  return {
+    root: 'lib',
+    build: {
+        target: 'es2015',
+        outDir: '../dist',
+    },
+    server: {
+      port: 3002,
+    },
+    define: envWithProcessPrefix,
+  }
+});
